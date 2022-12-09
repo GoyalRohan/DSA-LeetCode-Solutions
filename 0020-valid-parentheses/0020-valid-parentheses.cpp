80b2a230b258811f4e3b97e203aa7ca3a63482a9
Rohan Goyal <rohangoyal991@gmail.com>
2022-12-09 16:04:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (80.32%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       
+        bool ans = true ; 
+        stack<char> st ;
+        
+        for(int i=0 ; i<s.size() ; i++)
+        {
+            if(s[i] == '(' || s[i] =='{' || s[i] == '[' )
+                st.push(s[i]) ; 
+            else if(s[i] == ')')
+            {
+                if(!st.empty() && st.top() == '(')
+                    st.pop() ; 
+                else
+                {
+                    ans = false ; 
+                    break ; 
+                }
+            }
+            else if(s[i] == '}')
+            {
+                if(!st.empty() && st.top() == '{')
+                    st.pop() ; 
+                else
+                {
+                    ans = false ; 
+                    break ; 
+                }
+            }
+            else if( s[i] == ']')
+            {
+                if(!st.empty() &&st.top() == '[')
+                    st.pop() ; 
+                else
+                {
+                    ans = false ; 
+                    break ; 
+                }
+            }
+        }
+        
+        if(!st.empty())
+            return false ; 
+        else
+            return ans ; 
+        
+    }
+};
