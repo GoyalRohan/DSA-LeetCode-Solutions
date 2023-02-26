cf5ae6a6702caecf1877e14e827d1aba1acaedd4
Rohan Goyal <rohangoyal991@gmail.com>
2023-02-26 10:54:00 +0530

    Time: 13 ms (73.32%), Space: 16 MB (59.53%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,18 +4,47 @@ public:
         
         vector<int> ans ; 
         int n= nums.size() ; 
-        unordered_map<int, int> mpp ; 
         
-        for(int i=0 ; i<nums.size() ; i++)
+        int num1 = -1 , num2 = -1 , c1 = 0 , c2 = 0 ; 
+        
+        for(int i=0 ; i<n ; i++)
         {
-            mpp[nums[i]]++ ; 
+            if(nums[i] == num1)
+                c1++ ; 
+            else if(nums[i] == num2)
+                c2++ ; 
+            else if(c1 == 0)
+            {
+                c1 = 1 ; 
+                num1 = nums[i] ; 
+            }
+            else if(c2 == 0)
+            {
+                c2 = 1 ; 
+                num2 = nums[i] ; 
+            }
+            
+            else
+            {
+                c1-- ; c2-- ;
+            }
+            
         }
         
-        for(auto m : mpp)
+        c1 = 0 ; c2 = 0 ; 
+        
+        for(int i=0 ; i<n ; i++)
         {
-            if(m.second > n/3)
-                ans.push_back(m.first) ; 
+            if(nums[i] == num1)
+                c1++ ; 
+            else if(nums[i] == num2)
+                c2++ ; 
         }
+        
+        if(c1 > n/3)
+            ans.push_back(num1); 
+        if(c2 > n/3)
+            ans.push_back(num2) ; 
         
         return ans ; 
     }
