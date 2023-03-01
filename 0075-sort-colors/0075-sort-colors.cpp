5f233c6ce6800ffa5748e94421945077b791cf16
Rohan Goyal <rohangoyal991@gmail.com>
2023-03-01 07:57:28 +0530

    Time: 6 ms (23.78%), Space: 8.4 MB (31.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size() ;
+        vector<int> freq(3,0) ; 
+        
+        
+        for(int i=0 ; i<n ; i++)
+        {
+            freq[nums[i]]++ ; 
+        }
+        
+        int j = 0 ; 
+        for(int i=0 ; i<=2 ; i++)
+        {
+            while(freq[i] > 0)
+            {
+                nums[j++] = i ; 
+                freq[i]-- ; 
+            }
+        }
+        
+    }
+};
