82ac05c720c6b3a0c72c87420b3fca1baaa7b937
Rohan Goyal <rohangoyal991@gmail.com>
2023-03-28 12:12:13 +0530

    Time: 37 ms (5.02%), Space: 19.4 MB (6.48%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     
-    int solve(int i , int validDay , vector<int>& days, vector<int>& costs , vector<vector<int>> &dp)
-    {
-        if(i >= days.size())
-            return 0 ; 
-        
-        if(dp[i][validDay] != -1)
-            return dp[i][validDay] ; 
-        
-        if(days[i] > validDay)
-        {
-            int one = costs[0] + solve(i+1 , days[i] + 0 , days , costs , dp ) ; 
-            int seven = costs[1] + solve(i+1 , days[i] + 6 , days ,costs , dp) ; 
-            int thirty = costs[2] + solve(i+1 , days[i] + 29 , days , costs , dp) ; 
-            
-            int ans = min(one , seven) ; 
-            ans = min(ans , thirty) ; 
-            return dp[i][validDay] = ans ; 
-            
-        }
-        else
-            return dp[i][validDay] = 0 + solve(i+1 , validDay , days , costs, dp) ; 
-    }
-    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size() ; 
-        vector<vector<int>> dp(n , vector<int>( 400 , -1)) ; 
-        return solve(0 , 0 , days , costs , dp) ; 
+        vector<vector<int>> dp(n+1 , vector<int>( 400 , 0)) ; 
+        
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            for(int validDay= 0 ; validDay <= days[i]+29 ; validDay++ )
+            {
+                if(days[i] > validDay)
+                {
+                    int one = costs[0] + dp[i+1][days[i] + 0] ; 
+                    int seven = costs[1] + dp[i+1][days[i] + 6] ; 
+                    int thirty = costs[2] + dp[i+1][days[i] + 29] ; 
+
+                    int ans = min(one , seven) ; 
+                    ans = min(ans , thirty) ; 
+                    dp[i][validDay] = ans ; 
+
+                }
+                else
+                    dp[i][validDay] = 0 + dp[i+1][validDay] ;
+            }
+        }
+        
+        
+        return dp[0][0] ; 
     }
 };
