b832e8672fc27e9ebe5a54ca48b6f9fd4cec6aeb
Rohan Goyal <rohangoyal991@gmail.com>
2023-03-28 12:41:10 +0530

    Time: 3 ms (80.92%), Space: 9.7 MB (57.33%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     
+    int solve(int index ,vector<int>& days, vector<int>& costs , vector<int> &dp)
+    {
+        if(index >= days.size())
+            return 0 ; 
+        
+        if(dp[index] != -1)
+            return dp[index] ; 
+        
+        int one = costs[0] + solve(index+1 , days , costs , dp ) ; 
+        
+        int i; 
+        
+        for(i= index; i<days.size() && days[i] < days[index] + 7 ; i++) ; 
+            
+        int seven = costs[1] + solve(i, days ,costs , dp) ; 
+        
+        for(i= index; i<days.size() && days[i] < days[index] + 30 ; i++) ; 
+            
+        int thirty = costs[2] + solve(i, days ,costs , dp) ; 
+        
+        return dp[index] = min(one, min (seven, thirty)) ; 
+        
+    }
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size() ; 
-        vector<vector<int>> dp(n+1 , vector<int>( 400 , 0)) ; 
-        
-        for(int i=n-1 ; i>=0 ; i--)
-        {
-            for(int validDay= 0 ; validDay <= days[i]+29 ; validDay++ )
-            {
-                if(days[i] > validDay)
-                {
-                    int one = costs[0] + dp[i+1][days[i] + 0] ; 
-                    int seven = costs[1] + dp[i+1][days[i] + 6] ; 
-                    int thirty = costs[2] + dp[i+1][days[i] + 29] ; 
-
-                    int ans = min(one , seven) ; 
-                    ans = min(ans , thirty) ; 
-                    dp[i][validDay] = ans ; 
-
-                }
-                else
-                    dp[i][validDay] = 0 + dp[i+1][validDay] ;
-            }
-        }
-        
-        
-        return dp[0][0] ; 
+        vector<int> dp(n+1 , -1) ; 
+        return solve(0 , days , costs , dp) ; 
     }
 };
