121197500719870173502ffa0812edf41a5c91e4
Rohan Goyal <rohangoyal991@gmail.com>
2023-03-29 10:41:24 +0530

    Time: 35 ms (51.81%), Space: 20.8 MB (49.34%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     
-    int solve(int i, int count , vector<int>& satisfaction , vector<vector<int>> &dp)
-    {
-        if(i == satisfaction.size())   
-        {
-            return 0 ; 
-        }
-        
-        if(dp[i][count] != -1)
-            return dp[i][count] ; 
-        
-        int notTake = 0 + solve(i+1, count, satisfaction , dp) ; 
-        
-        int take = satisfaction[i] * count + solve(i+1 , count+1 , satisfaction , dp) ;
-        
-        return dp[i][count] = max(take, notTake) ; 
-        
-    }
-    
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size() ; 
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1)) ; 
+        vector<vector<int>> dp(n+1 , vector<int>(n+2 , 0)) ; 
         sort(satisfaction.begin() , satisfaction.end()) ; 
-        return max(0 , solve(0 , 1 , satisfaction , dp)) ; 
+        int maxi = 0 ; 
+        
+        
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            for(int count=i+1 ; count>=1 ; count--)
+            {
+                int notTake = 0 + dp[i+1][count-1] ; 
+        
+                int take = satisfaction[i] * count + dp[i+1][count] ;
+                dp[i][count-1] = max(take, notTake) ; 
+                
+            }
+        }
+        
+        
+        return dp[0][0] ;
     }
 };
