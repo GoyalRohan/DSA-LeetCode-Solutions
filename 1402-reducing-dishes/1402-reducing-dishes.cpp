f0b266b95fedee7542ca3c966424cd8805e7add2
Rohan Goyal <rohangoyal991@gmail.com>
2023-03-29 10:43:57 +0530

    Time: 37 ms (49.16%), Space: 20.8 MB (54.63%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -5,17 +5,16 @@ public:
         int n = satisfaction.size() ; 
         vector<vector<int>> dp(n+1 , vector<int>(n+2 , 0)) ; 
         sort(satisfaction.begin() , satisfaction.end()) ; 
-        int maxi = 0 ; 
-        
+
         
         for(int i=n-1 ; i>=0 ; i--)
         {
-            for(int count=i+1 ; count>=1 ; count--)
+            for(int count=i ; count>=0 ; count--)
             {
-                int notTake = 0 + dp[i+1][count-1] ; 
+                int notTake = 0 + dp[i+1][count] ; 
         
-                int take = satisfaction[i] * count + dp[i+1][count] ;
-                dp[i][count-1] = max(take, notTake) ; 
+                int take = satisfaction[i] * (count+1) + dp[i+1][count+1] ;
+                dp[i][count] = max(take, notTake) ; 
                 
             }
         }
