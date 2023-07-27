e868f759e9af7115372b03e9db13f07c83b5e842
Rohan Goyal <rohangoyal991@gmail.com>
2023-07-27 09:06:25 +0530

    Time: 7 ms (26.40%), Space: 11.9 MB (81.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,37 +11,52 @@
  */
 class Solution {
 public:
+    
+    void solve(TreeNode *root , int level , vector<int> &ans)
+    {
+        if(root == NULL)
+            return  ; 
+        
+        if(ans.size() == level)
+            ans.push_back(root->val) ; 
+        
+        solve(root->right , level+1 , ans) ; 
+        solve(root->left , level+1 , ans) ; 
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
-        map<int , int> mpp ; 
+//         map<int , int> mpp ; 
         vector<int> ans ; 
-        queue<pair<TreeNode* , int>> q ; 
+//         queue<pair<TreeNode* , int>> q ; 
         
         if(!root)
             return ans ; 
         
-        q.push({root , 0}) ; 
+//         q.push({root , 0}) ; 
         
-        while(!q.empty())
-        {
-            auto p = q.front()  ;q.pop() ;
-            TreeNode *node = p.first ; 
-            int level = p.second ; 
+//         while(!q.empty())
+//         {
+//             auto p = q.front()  ;q.pop() ;
+//             TreeNode *node = p.first ; 
+//             int level = p.second ; 
             
-            if(mpp.find(level) == mpp.end())
-            {
-                mpp[level] = node->val ; 
-            }
+//             if(mpp.find(level) == mpp.end())
+//             {
+//                 mpp[level] = node->val ; 
+//             }
             
-            if(node->right)
-                q.push({node->right , level+1}) ; 
-            if(node->left)
-                q.push({node->left , level+1}) ;
-        }
+//             if(node->right)
+//                 q.push({node->right , level+1}) ; 
+//             if(node->left)
+//                 q.push({node->left , level+1}) ;
+//         }
         
-        for(auto m : mpp)
-        {
-            ans.push_back(m.second) ; 
-        }
+//         for(auto m : mpp)
+//         {
+//             ans.push_back(m.second) ; 
+//         }
+        
+        solve(root , 0 , ans) ; 
         
         return ans ; 
     }
