0c7451da7e5efc0736849bbafc8af984722f8f39
Rohan Goyal <rohangoyal991@gmail.com>
2023-07-28 12:03:49 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (82.98%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     
-    void solve(int open , int closed , string temp , vector<string> &ans)
+    void solve(int open , int closed , string &temp , vector<string> &ans)
     {
         
-        if(open == 0)
+        if(open > closed || open < 0)
+            return  ; 
+        
+        if(open + closed == 0)
         {
-            while(closed > 0)
-            {
-                temp += ")" ; 
-                closed-- ; 
-            }
-            
             ans.push_back(temp) ; 
             return ; 
         }
         
-        // temp += "(" ; 
-        solve(open-1 , closed , temp + "(" , ans) ; 
-        // temp.pop_back() ; 
+        temp += "(" ; 
+        solve(open-1 , closed , temp , ans) ; 
+        temp.pop_back() ; 
         
-        if(open < closed)
-        {
-            // temp+= ")" ; 
-            solve(open , closed-1 , temp + ")", ans) ; 
-            // temp.pop_back() ; 
-        }
+            temp+= ")" ; 
+            solve(open , closed-1 , temp , ans) ; 
+            temp.pop_back() ; 
+
             
     }
     
