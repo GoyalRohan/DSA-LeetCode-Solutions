0424588a5ee7739d47d08d8a862e958c4552d16e
Rohan Goyal <rohangoyal991@gmail.com>
2023-07-28 12:06:21 +0530

    Time: 4 ms (67.91%), Space: 20.1 MB (7.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     
-    void solve(int open , int closed , string &temp , vector<string> &ans)
-    {
+//     void solve(int open , int closed , string &temp , vector<string> &ans)
+//     {
         
-        if(open > closed || open < 0)
-            return  ; 
+//         if(open > closed || open < 0)
+//             return  ; 
         
-        if(open + closed == 0)
-        {
-            ans.push_back(temp) ; 
-            return ; 
-        }
+//         if(open + closed == 0)
+//         {
+//             ans.push_back(temp) ; 
+//             return ; 
+//         }
         
-        temp += "(" ; 
-        solve(open-1 , closed , temp , ans) ; 
-        temp.pop_back() ; 
+//         temp += "(" ; 
+//         solve(open-1 , closed , temp , ans) ; 
+//         temp.pop_back() ; 
         
-            temp+= ")" ; 
-            solve(open , closed-1 , temp , ans) ; 
-            temp.pop_back() ; 
+//             temp+= ")" ; 
+//             solve(open , closed-1 , temp , ans) ; 
+//             temp.pop_back() ; 
 
             
+//     }
+    
+    void solve(int open , int closed , string temp , vector<string> &ans)
+    {
+        if(open == 0 && closed == 0)
+        {
+            ans.push_back(temp) ; 
+            return ;
+        }
+        
+        if(open >= 0)
+            solve(open-1 , closed , temp + "(" , ans) ; 
+        
+        if(closed > open)
+            solve(open , closed-1 , temp + ")" , ans) ; 
     }
     
     vector<string> generateParenthesis(int n) {
