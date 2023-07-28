8c91cdb8c5f4fa92079b34323d17450c5fb7b802
Rohan Goyal <rohangoyal991@gmail.com>
2023-07-28 17:38:22 +0530

    Time: 3 ms (70.47%), Space: 7.6 MB (43.52%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:    
     
-    int solve(int i, int j , vector<int> &nums, vector<vector<int>> &dp)
+    // ONE METHOD 
+//     int solve(int i, int j , vector<int> &nums, vector<vector<int>> &dp)
+//     {
+//         if(i > j)
+//             return 0 ; 
+        
+//         if(i==j)
+//             return nums[i] ; 
+        
+//         if(dp[i][j] != -1)
+//             return dp[i][j] ; 
+        
+//         int take_i = nums[i] + min(solve(i+2 , j , nums , dp) , solve(i+1 , j-1, nums, dp)) ; 
+//         int take_j = nums[j] + min(solve(i+1 , j-1 , nums, dp) , solve(i , j-2 ,nums, dp)) ; 
+        
+//         return dp[i][j] = max(take_i , take_j) ; 
+//     }
+    
+    
+    // SECOND METHOD
+    
+    int solve(int i ,int j , vector<int> &nums, vector<vector<int>> &dp)
     {
         if(i > j)
             return 0 ; 
         
-        if(i==j)
-            return nums[i] ; 
+        if(i == j)
+            return nums[i] ;
         
         if(dp[i][j] != -1)
             return dp[i][j] ; 
         
-        int take_i = nums[i] + min(solve(i+2 , j , nums , dp) , solve(i+1 , j-1, nums, dp)) ; 
-        int take_j = nums[j] + min(solve(i+1 , j-1 , nums, dp) , solve(i , j-2 ,nums, dp)) ; 
+        int take_i = nums[i] - solve(i+1 , j , nums , dp) ; 
+        int take_j = nums[j] - solve(i , j-1 , nums , dp) ; 
         
-        return dp[i][j] = max(take_i , take_j) ; 
+        return dp[i][j] =  max(take_i , take_j) ;
     }
     
     bool PredictTheWinner(vector<int>& nums) {
@@ -25,9 +46,11 @@ public:
         
         int n = nums.size() ; 
         vector<vector<int>> dp(n , vector<int>(n , -1)) ; 
-        int play1= solve(0 , n-1 , nums , dp) ; 
-        int play2 = score - play1 ; 
+        // int play1= solve(0 , n-1 , nums , dp) ; 
+        // int play2 = score - play1 ; 
+        // return play1 >= play2 ; 
         
-        return play1 >= play2 ; 
+        return solve(0 , n-1 , nums , dp) >= 0 ; 
+        
     }
 };
