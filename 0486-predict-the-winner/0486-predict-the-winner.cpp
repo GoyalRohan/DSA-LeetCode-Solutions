ce5d4fdb2349a66abadbfd1e5ac71f5a41f1c316
Rohan Goyal <rohangoyal991@gmail.com>
2023-07-28 17:33:30 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
+public:    
     
-    int solve(int i, int j , vector<int> &nums)
+    int solve(int i, int j , vector<int> &nums, vector<vector<int>> &dp)
     {
         if(i > j)
             return 0 ; 
@@ -9,10 +9,13 @@ public:
         if(i==j)
             return nums[i] ; 
         
-        int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1 , j-1, nums)) ; 
-        int take_j = nums[j] + min(solve(i+1 , j-1 , nums) , solve(i , j-2 ,nums)) ; 
+        if(dp[i][j] != -1)
+            return dp[i][j] ; 
         
-        return max(take_i , take_j) ; 
+        int take_i = nums[i] + min(solve(i+2 , j , nums , dp) , solve(i+1 , j-1, nums, dp)) ; 
+        int take_j = nums[j] + min(solve(i+1 , j-1 , nums, dp) , solve(i , j-2 ,nums, dp)) ; 
+        
+        return dp[i][j] = max(take_i , take_j) ; 
     }
     
     bool PredictTheWinner(vector<int>& nums) {
@@ -21,7 +24,8 @@ public:
             score += n ;
         
         int n = nums.size() ; 
-        int play1= solve(0 , n-1 , nums) ; 
+        vector<vector<int>> dp(n , vector<int>(n , -1)) ; 
+        int play1= solve(0 , n-1 , nums , dp) ; 
         int play2 = score - play1 ; 
         
         return play1 >= play2 ; 
