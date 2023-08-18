890bc2407615cb2894e76e30455fc782258f1aa0
Rohan Goyal <rohangoyal991@gmail.com>
2023-08-18 16:29:54 +0530

    Time: 149 ms (15.73%), Space: 43.7 MB (10.26%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        
+        vector<int> adj(n) ; 
+        map<int , multiset<int>> mpp ; 
+        
+        for(auto road : roads)
+        {
+            int u = road[0] , v= road[1] ; 
+            mpp[u].insert(v) ; 
+            mpp[v].insert(u) ; 
+            // adj[u].push_back(v) ; 
+            // adj[v].push_back(u) ; 
+        }
+        
+        int maxi = 0 ; 
+        for(int i=0 ; i<n ; i++)
+        {
+            for(int j=0 ; j<n ; j++)
+            {
+                if(i == j)
+                    continue ; 
+                
+                int sum = mpp[i].size() + mpp[j].size() ; 
+                
+                if(mpp[i].find(j) != mpp[i].end())
+                    sum-- ;
+                
+                maxi = max(maxi , sum) ; 
+            }
+        }
+        
+        return maxi ; 
+    }
+};
