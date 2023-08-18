c75617b0c8325fa487705cbc1eabd53f08320d88
Rohan Goyal <rohangoyal991@gmail.com>
2023-08-18 16:33:57 +0530

    Time: 117 ms (34.23%), Space: 42.7 MB (11.69%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -3,7 +3,7 @@ public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         
         vector<int> adj(n) ; 
-        map<int , multiset<int>> mpp ; 
+        unordered_map<int , unordered_set<int>> mpp ; 
         
         for(auto road : roads)
         {
