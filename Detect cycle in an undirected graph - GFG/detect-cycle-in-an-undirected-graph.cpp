28f7d2bc6880b0734e4921f30e4186ba593abb2e
Rohan Goyal <rohangoyal991@gmail.com>
2023-08-22 09:16:49 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,38 +6,45 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
+    
+    bool check(int i , vector<int> &vis , vector<int> adj[])
+    {
+        queue<pair<int , int>> q ; 
+        q.push({i , -1}) ; 
+        vis[i] = 1 ; 
+        
+        while(!q.empty())
+        {
+            int node = q.front().first ; 
+            int par = q.front().second ; 
+            q.pop() ; 
+            
+            for(auto it : adj[node])
+            {
+                if(!vis[it])
+                {
+                    vis[it] = 1 ; 
+                    q.push({it , node}) ;
+                }
+                else if(it != par)
+                    return true ; 
+            }
+        }
+        
+        return false ; 
+        
+    }
+    
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         vector<int> vis(V , 0) ; 
-        
-        
         for(int i=0 ; i<V ; i++)
         {
-            if(vis[i] == 0)
-            {
-                queue<pair<int, int>> q ; 
-                q.push({i , -1}) ; 
-                vis[i] = 1 ; 
-                
-                while(!q.empty())
-                {
-                    int node = q.front().first ; 
-                    int par = q.front().second ; 
-                    q.pop() ; 
-                    
-                    for(auto it : adj[node])
-                    {
-                        if(vis[it] == 0)
-                        {
-                            vis[it] = 1 ; 
-                            q.push({it , node}) ; 
-                        }
-                        else if(it != par)
-                            return true ; 
-                    }
-                }
-            }
+            if(!vis[i])
+                if(check(i , vis , adj)) 
+                    return true ;  
         }
+        
         return false ; 
     }
 };
