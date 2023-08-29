612945d26ade19b203d95697b49d9b0dac895bfa
Rohan Goyal <rohangoyal991@gmail.com>
2023-08-29 12:18:10 +0530

    Time: 20 ms (91.38%), Space: 10.7 MB (86.10%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,89 +1,23 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        
-        // BRUTE APPROACH
-//         int ans  = -1  , mini = INT_MAX ; 
-//         int n = customers.size() ; 
-        
-//         for(int i=0 ; i<=n ; i++)
-//         {
-//             int penalty = 0 ; 
-//             for(int j=0 ; j<n ; j++)
-//             {
-//                if(i>j)
-//                {
-//                    if(customers[j] == 'N')
-//                        penalty++ ; 
-//                }
-//                else
-//                {
-//                    if(customers[j] == 'Y')
-//                        penalty++ ; 
-//                }
-//             }
-            
-//             // cout<<i<<" , "<<penalty<<endl ; 
-//             if(mini > penalty)
-//             {
-//                 mini = penalty ; 
-//                 // cout<<penalty<<" , "<<i<<endl ; 
-//                 ans = i ; 
-//             }
-//         }
-        
-//         return ans ; 
-        
-        // OPTIMIZED APPROACH
-        
         int n = customers.size() ; 
-        vector<int> prefix(n+1 , 0) , suffix(n, 0) ; 
-        int penalty = 0 ; 
+        int ind = -1 , score = 0 , mxscore = 0 ; 
         
         for(int i=0 ; i<n ; i++)
-        {
-            if(customers[i] == 'N')
-            {
-                penalty++ ; 
-                
-            }
-            prefix[i+1] = penalty ; 
-        }
-        
-        penalty = 0 ; 
-        
-        for(int i=n-1 ; i>=0 ; i--)
         {
             if(customers[i] == 'Y')
+                score++ ; 
+            else
+                score-- ; 
+            
+            if(score > mxscore)
             {
-                penalty++ ; 
-                
-            }
-            suffix[i] = penalty ; 
-        }
-        
-        
-//         for(int i=0 ; i<n ; i++)
-//         {
-//             cout<<prefix[i]<<" , "<<suffix[i]<<endl ;
-//         }
-        
-        int mini = INT_MAX ;
-        int ans = -1 ; 
-        
-        for(int i=0 ; i<n ; i++)
-        {
-            int sum = prefix[i] + suffix[i] ; 
-            if(sum < mini)
-            {
-                mini = sum ; 
-                ans = i ; 
+                mxscore = score ; 
+                ind = i ; 
             }
         }
         
-        if(mini > prefix[n])
-            ans = n ; 
-        
-        return ans ; 
+        return ind+ 1; 
     }
 };
