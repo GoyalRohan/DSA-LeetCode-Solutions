5fa8ee79b8bac37cdd57b459210b2f51e75ea978
Rohan Goyal <rohangoyal991@gmail.com>
2023-08-30 15:39:18 +0530

    Time: 173 ms (6.31%), Space: 49.8 MB (5.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist1(n , 1e9) ; 
-        dist1[src] = 0 ; 
-        
+        vector<int> dist(n , INT_MAX) ;
+        dist[src] = 0 ; 
         
         for(int i=1 ; i<=k+1 ; i++)
         {
-            vector<int> dist2 = dist1 ; 
-            for(auto edge : flights)
+            vector<int> tempdist = dist ; 
+            for(auto flight : flights)
             {
-                int u = edge[0] , v = edge[1] , wt = edge[2]; 
-                if(dist1[u] + wt < dist2[v])
-                    dist2[v] = dist1[u] + wt ; 
-
-            }   
+                int u = flight[0] , v = flight[1] , cost = flight[2] ; 
+                if(dist[u] != INT_MAX && dist[u] + cost < tempdist[v])
+                    tempdist[v] = dist[u] + cost ; 
+            }
             
-            dist1 = dist2 ; 
+            dist = tempdist ; 
         }
         
-        if(dist1[dst] == 1e9)
-            return -1 ; 
+        if(dist[dst] != INT_MAX)
+            return dist[dst] ;
         
-        return dist1[dst] ; 
-        
+        return -1 ; 
     }
 };
