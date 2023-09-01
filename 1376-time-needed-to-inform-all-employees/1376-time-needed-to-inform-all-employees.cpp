9722f2830f8b6b65e450fc65d0ef5270c4b188a7
Rohan Goyal <rohangoyal991@gmail.com>
2023-09-01 17:00:01 +0530

    Time: 220 ms (80.08%), Space: 117.5 MB (71.64%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    
-    int mx = 0 , ans = 0 ; 
-    
-    void dfs(int man,vector<int>& informTime , vector<int> adj[])
-    {
-        mx = max(mx , ans) ; 
-        for(auto emp : adj[man])
-        {
-            ans += informTime[man] ; 
-            dfs(emp , informTime, adj) ; 
-            ans -= informTime[man] ; 
-        }
-    }
-    
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        int ans = 0 ;
         vector<int> adj[n] ; 
-        vector<int> vis(n , 0) ; 
         
         for(int i=0 ; i<n ; i++)
         {
-            if(i != headID)
-            {
-                int man = manager[i] ; 
-                adj[man].push_back(i) ;
-            }
-             
+            // cout<<manager[i]<<" " ; 
+            if(manager[i] != -1)
+                adj[manager[i]].push_back(i) ; 
+        }
+            
+        // for(int i=0; i<n ; i++)
+        // {
+        //     cout<<i <<":"; 
+        //     for(auto e : adj[i])
+        //         cout<<e<<" " ; 
+        //     cout<<endl ; 
+        // }
+        
+        queue<pair<int, int>> q ; 
+        q.push({headID , 0}) ; 
+        int ans = 0 ; 
+        
+        while(!q.empty())
+        {
+            
+                int emp = q.front().first , val = q.front().second; 
+                q.pop() ; 
+
+                for(int it : adj[emp])
+                {
+                    q.push({it , informTime[emp] + val}) ; 
+                    ans = max(ans , informTime[emp] + val) ; 
+                }
+            
         }
         
+        return ans ; 
         
-        dfs(headID, informTime , adj) ;
-
-        return mx ; 
     }
 };
