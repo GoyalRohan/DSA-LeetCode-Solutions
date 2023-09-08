617f09e9d2f2e55959330f32bc2b01224e16a743
Rohan Goyal <rohangoyal991@gmail.com>
2023-09-08 16:48:55 +0530

    Time: 72 ms (90.94%), Space: 62 MB (56.01%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
     
-    int solve(int i, int target, vector<int> &nums , int n, vector<int> &dp)
-    {
-        if(i >= n) return -1e9 ; 
-        if(i == n-1)
-            return 0 ;
+//     int solve(int i, int target, vector<int> &nums , int n, vector<int> &dp)
+//     {
+//         if(i >= n) return -1e9 ; 
+//         if(i == n-1)
+//             return 0 ;
         
-        if(dp[i] != -1)
-            return dp[i] ; 
+//         if(dp[i] != -1)
+//             return dp[i] ; 
         
-        int maxi = -1e9 ; 
-        for(int j=i+1 ; j<n ; j++)
-        {
-            if(abs(nums[j] - nums[i]) <= target)
-                maxi = max(maxi , 1 + solve(j , target , nums, n, dp)) ; 
-        } 
+//         int maxi = -1e9 ; 
+//         for(int j=i+1 ; j<n ; j++)
+//         {
+//             if(abs(nums[j] - nums[i]) <= target)
+//                 maxi = max(maxi , 1 + solve(j , target , nums, n, dp)) ; 
+//         } 
         
-        return dp[i] = maxi ; 
-    }
+//         return dp[i] = maxi ; 
+//     }
     
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size() ; 
-        vector<int> dp(n , -1) ; 
-        int ans = solve(0 , target , nums, n, dp) ; 
+        vector<int> dp(n , 0) ; 
+        
+        for(int i=n-2 ; i >=0 ; i--)
+        {
+            int maxi = -1e9 ; 
+            for(int j=i+1 ; j<n ; j++)
+            {
+                if(abs(nums[j] - nums[i]) <= target)
+                    maxi = max(maxi , 1 + dp[j]) ; 
+            } 
+
+            dp[i] = maxi ; 
+        }
+        
+        int ans = dp[0] ; 
         if(ans <= -1e8)
             return -1 ; 
         return ans ; 
