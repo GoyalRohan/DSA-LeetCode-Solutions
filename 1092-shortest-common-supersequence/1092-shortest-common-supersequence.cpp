432271c5610b51e0731ced2cd612b7aa64fedb9a
Rohan Goyal <rohangoyal991@gmail.com>
2023-09-22 09:05:16 +0530

    Time: 18 ms (73.21%), Space: 13.4 MB (19.27%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,63 +1,53 @@
 class Solution {
 public:
-    
-    
-    string shortestCommonSupersequence(string s, string t) {
-        int n =s.size() ; 
-        int m = t.size() ; 
-        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0)) ; 
-
-        for(int ind1=0 ; ind1<=n ; ind1++)
-            dp[ind1][0] = 0 ; 
-        for(int ind2=0 ; ind2<=m ; ind2++)
-            dp[0][ind2] = 0 ; 
-
-        for(int ind1=1 ; ind1<=n ; ind1++)
+    string shortestCommonSupersequence(string str1, string str2) {
+        string ans = "" ;
+        int n = str1.size() , m = str2.size() ; 
+        vector<vector<int>> dp(n+1, vector<int>(m+1 , 0)) ; 
+        
+        for(int i=1 ; i<=n ; i++)
         {
-            for(int ind2=1 ; ind2<=m ; ind2++)
+            for(int j=1; j<=m ; j++)
             {
-                if(s[ind1-1] == t[ind2-1])
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1] ;
+                if(str1[i-1] == str2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1] ; 
                 else
-                    dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2] , dp[ind1][ind2-1]) ; 
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]) ; 
             }
         }
-
-       string ans = "" ;
-       int i=n , j = m ; 
-       
-       while(i>0 && j >0)
-       {
-           if(s[i-1] == t[j-1])
-           {
-               ans += s[i-1] ; 
-               i-- ; j-- ; 
-           }
-           else if(dp[i-1][j] > dp[i][j-1])
-           {
-               ans += s[i-1] ; 
-               i-- ; 
-           }
-           else
-           {
-               ans += t[j-1] ; 
-               j-- ; 
-           }
-       }
+        
+        int i=n , j = m ; 
+        
+        while(i>0 && j>0)
+        {
+            if(str1[i-1] == str2[j-1])
+            {
+                ans += str1[i-1] ; 
+                i-- ; j-- ; 
+            }
+            else if(dp[i-1][j] > dp[i][j-1])
+            {
+                ans += str1[i-1] ; 
+                i-- ; 
+            }
+            else
+            {
+                ans += str2[j-1] ; 
+                j-- ; 
+            }
+        }
         
         while(i > 0)
         {
-            ans += s[i-1] ; 
-            i-- ; 
+            ans += str1[i-1] ; i-- ; 
         }
         
         while(j > 0)
         {
-            ans += t[j-1] ; 
-            j-- ; 
+            ans += str2[j-1] ; j-- ; 
         }
-           
-        reverse(ans.begin() ,ans.end()) ; 
+        
+        reverse(ans.begin(), ans.end()) ; 
         return ans ; 
     }
 };
