c1c60a72d009ee660246b7f60055acce7bcd0829
Rohan Goyal <rohangoyal991@gmail.com>
2023-09-30 11:36:32 +0530

    Time: 7 ms (85.34%), Space: 12.4 MB (74.29%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int deleteAndEarn(vector<int>& nums) {
-        int n = nums.size() ; 
-        int maxi = *max_element(nums.begin() , nums.end()) ; 
-        int dp[200005] ; 
-        vector<int> freq(200005 , 0) ; 
+    
+    int solve(int i, vector<int> &nums, vector<int> &dp)
+    {
+        if(i == nums.size())
+            return 0 ; 
         
-        for(auto nu : nums)
-            freq[nu]++ ; 
+        if(dp[i] != -1)
+            return dp[i] ; 
         
-        dp[1]= freq[1] ; 
-        dp[2]= max(dp[1] , 2*freq[2]) ; 
-        
-        for(int i=3 ; i<=maxi ; i++)
+        int curvalue = nums[i] ; 
+        int cursum = nums[i] ; 
+        int index = i+1 ; 
+        while(index < nums.size() && curvalue == nums[index])
         {
-            dp[i] = max(dp[i-1] , dp[i-2] + i*freq[i]) ; 
+            cursum += curvalue ; 
+            index++ ; 
         }
         
-        return dp[maxi] ;
+        while(index < nums.size() && curvalue + 1 == nums[index])
+        {
+            index++ ; 
+        }
         
+        return dp[i] = max(cursum + solve(index , nums , dp) , solve(i+1 , nums , dp)) ; 
+        
+    }
+    
+    int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size() ; 
+        sort(nums.begin() , nums.end()) ; 
+        
+        vector<int> dp(n , -1) ; 
+        return solve(0 , nums , dp) ; 
     }
 };
