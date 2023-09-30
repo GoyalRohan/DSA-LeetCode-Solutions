87744b22a7b2c52e542685a00748edff30172566
Rohan Goyal <rohangoyal991@gmail.com>
2023-09-30 09:09:22 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (32.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    map<string , char> mpp; 
-    
-    int solve(int i, int n, string s , map<string , char> &mpp, vector<int> &dp)
-    {
-        if(i == n)
-            return 1 ; 
-        
-        if(dp[i] != -1)
-            return dp[i] ; 
-        
-        string temp = "" ; 
-        int ans = 0 ; 
-        
-        for(int ind=i ; ind < min(i+2 , n) ; ind++)
-        {
-            temp += s[ind]  ;
-            // if(mpp.find(temp) == mpp.end())
-            //     break ; 
-            
-            if(stoi(temp) == 0 || stoi(temp) > 26)
-                break  ;
-            
-            ans += solve(ind+1 , n , s , mpp , dp) ; 
-        }
-        
-        return dp[i] = ans ; 
-    }
-    
+
     int numDecodings(string s) {
         
         int n = s.size() ; 
-        // for(int i=0 ; i<26 ; i++)
-        // {
-        //     mpp[to_string(i+1)] = 'A' + i ; 
-        // }
+        vector<int> dp(n+1 , 0) ;
         
-        vector<int> dp(n , -1) ; 
-        return solve(0 , n , s , mpp , dp) ; 
+        dp[n] = 1 ; 
+        
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            string temp = "" ; 
+            int ans = 0 ; 
+        
+            for(int ind=i ; ind < min(i+2 , n) ; ind++)
+            {
+                temp += s[ind]  ;
+                if(stoi(temp) == 0 || stoi(temp) > 26)
+                    break  ;
+
+                ans += dp[ind+1] ; 
+            }
+
+            dp[i] = ans ;    
+        }
+        
+        
+        
+        return dp[0] ; 
         
         
     }
