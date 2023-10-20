460c5ff0d6afc073e6088ad116ad9b4cf3a2f22f
Rohan Goyal <rohangoyal991@gmail.com>
2023-10-20 09:02:06 +0530

    Time: 62 ms (78.14%), Space: 29.2 MB (78.47%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    bool canCross(vector<int>& stones) {
+    
+    map<pair<int, int> , bool> mpp ; 
+    
+    bool solve(int pos, int prevjump , int n, unordered_set<int> &s , vector<int>& stones)
+    {
+        if(pos > stones[n-1] || s.find(pos) == s.end())
+            return false ; 
         
-        int n = stones.size() ; 
+        if(pos == stones[n-1])
+            return true ; 
         
-        unordered_map<int, unordered_set<int>> mpp ; 
+        if(mpp.find({pos, prevjump}) != mpp.end())
+            return mpp[{pos, prevjump}] ; 
         
-        mpp[stones[0]+1].insert(1) ; 
-        
-        for(int i=1 ; i<n ; i++)
+        for(int jump=-1 ; jump<=1 ;jump++)
         {
-            int position = stones[i] ; 
-            for(auto it : mpp[position])
+            if(prevjump+jump > 0)
             {
-                mpp[position+it].insert(it) ; 
-                mpp[position+it+1].insert(it+1) ; 
-                mpp[position+it-1].insert(it-1) ; 
+                if(solve(pos+prevjump+jump , prevjump + jump , n , s , stones)) 
+                    return  mpp[{pos, prevjump}] = true ; 
             }
         }
         
-        return mpp[stones.back()].size() != 0 ;
+        return  mpp[{pos, prevjump}] = false ;
+    }
+    
+    bool canCross(vector<int>& stones) {
+        if(stones[1] - stones[0] > 1)
+            return false ; 
+        
+        int n = stones.size() ; 
+        
+        unordered_set<int> s(stones.begin(), stones.end()) ; 
+        
+        return solve(1 , 1 , n, s , stones) ;
     }
 };
