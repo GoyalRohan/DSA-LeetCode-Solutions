db0e434bdea84fa0e9ecdc7ff75d8df859e962ff
Rohan Goyal <rohangoyal991@gmail.com>
2023-10-23 12:57:55 +0530

    Time: 7 ms (94.26%), Space: 11.7 MB (6.68%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -5,13 +5,11 @@ public:
         int sum = 0 ; 
         
         for(int i=0 ; i<n ; i++)
-        {
-            sum += mat[i][i] + mat[i][n-1-i] ; 
-        }
+            sum += mat[i][i] + mat[i][n-i-1] ; 
         
         if(n%2 == 0)
             return sum ; 
-        else
-            return sum - mat[n/2][n/2] ; 
+        
+        return sum - mat[n/2][n/2] ;
     }
 };
