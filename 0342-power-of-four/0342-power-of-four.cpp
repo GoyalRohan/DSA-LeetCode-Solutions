740e225239935a1b353708ba0eab26a042af7ec5
Rohan Goyal <rohangoyal991@gmail.com>
2023-10-23 12:13:13 +0530

    Time: 5 ms (10.09%), Space: 6.2 MB (47.70%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -4,14 +4,15 @@ public:
     bool isPowerOfFour(int n) {
         
         if(n<1)
-            return false ;
+            return false ; 
         
-       for(int i=0 ; i<32 ; i+=2)
-       {
-           if(n == (1<<i))
-               return true ; 
-       }
+       while(n > 1)
+        {
+            if(n%4 != 0)
+                return false ; 
+            n = n/4 ; 
+        }
         
-       return false ; 
+        return true ;
     }
 };
