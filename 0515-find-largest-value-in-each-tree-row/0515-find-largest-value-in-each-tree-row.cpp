18dd21ea5d83315816a76d523f5892522a67cf4e
Rohan Goyal <rohangoyal991@gmail.com>
2023-10-24 11:01:54 +0530

    Time: 4 ms (91.88%), Space: 22.6 MB (19.92%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -15,28 +15,36 @@ public:
     map<int, int> mpp ; 
     vector<int> ans ; 
     
-    void preorder(TreeNode *root, int level)
-    {
-        if(root == NULL)
-            return ; 
-        
-        if(mpp.find(level) == mpp.end())
-            mpp[level] = root->val ; 
-        else
-            mpp[level] = max(mpp[level] , root->val) ; 
-        
-        preorder(root->left , level+1) ; 
-        preorder(root->right, level+1) ; 
-    }
     
     vector<int> largestValues(TreeNode* root) {
         
         if(root == NULL)
             return ans ; 
         
-        preorder(root, 0) ; 
-        for(auto m : mpp)
-            ans.push_back(m.second) ; 
+        
+        queue<TreeNode*> q ; 
+        q.push(root) ; 
+        
+        while(!q.empty())
+        {
+            int size = q.size() ; 
+            int maxi = INT_MIN ; 
+            
+            while(size-- > 0)
+            {
+                TreeNode* node = q.front() ; 
+                q.pop() ; 
+                
+                maxi = max(maxi , node->val) ; 
+                
+                if(node->left)
+                    q.push(node->left) ; 
+                if(node->right)
+                    q.push(node->right) ; 
+            }
+            
+            ans.push_back(maxi) ; 
+        }
         
         return ans ; 
     }
