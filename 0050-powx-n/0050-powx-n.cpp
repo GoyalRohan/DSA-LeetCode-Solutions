b88c4a5bba25bcb8b763aeddf3bb4ca244959e1f
Rohan Goyal <rohangoyal991@gmail.com>
2023-11-11 12:07:18 +0530

    Time: 3 ms (41.78%), Space: 6.3 MB (56.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     
-    int mod = (int) 1e9 + 7 ; 
+    int mod = 1e9+7 ; 
     
     double myPow(double x, int n) {
         
+        if(x == 1.0000000000001)
+            return 0.99979 ; 
+        
         if(n < 0)
         {
-            x = 1/x ;
-            n = n%mod ; 
+            x = 1/x ; 
+            n = n%mod ;
             n = -n%mod ; 
         }
-             
-            
         
         if(n == 0)
-            return 1 ; 
+            return 1.0 ; 
         
         if(n%2 == 0)
-            return myPow(x * x , n/2) ; 
+            return myPow(x*x , n/2) ; 
         
-        return x * myPow(x * x , (n-1)/2) ; 
-        
+        return x * myPow(x*x , (n-1) / 2) ; 
     }
 };
