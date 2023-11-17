38f49bd537072b6b2193cf4ff51e56b2cdc17516
Rohan Goyal <rohangoyal991@gmail.com>
2023-11-17 09:20:07 +0530

    Time: 141 ms (98.53%), Space: 113.3 MB (5.08%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -3,16 +3,30 @@ public:
     
     int minPairSum(vector<int>& nums) {
         int n= nums.size() ;
-        int low = 0 , high = n-1 ; 
+        int low = INT_MAX , high = INT_MIN ; 
         int ans = INT_MIN; 
-        sort(nums.begin(), nums.end()) ; 
         
-        while(low < high)
+        vector<int> freq(100001, 0) ;
+        
+        for(int i=0 ; i<n; i++)
         {
-            int cursum = nums[low] + nums[high] ; 
-            if(ans < cursum)
-                ans = cursum ; 
-            low++ ; high-- ; 
+            low = min(low , nums[i]) ; 
+            high = max(high , nums[i]) ; 
+            freq[nums[i]]++ ;
+        }
+        
+        while(low <= high)
+        {
+            if(freq[low] == 0)
+                low++ ; 
+            else if(freq[high] == 0)
+                high-- ; 
+            else
+            {
+                int cursum = low + high ; 
+                ans = max(ans, cursum) ; 
+                freq[low]-- ; freq[high]-- ; 
+            }
         }
         
         return ans ; 
