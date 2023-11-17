1258931e1264fa13f5b1386d6c797750f1e5242b
Rohan Goyal <rohangoyal991@gmail.com>
2023-11-17 15:04:38 +0530

    Time: 40 ms (42.60%), Space: 17.5 MB (16.61%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,46 +9,69 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int a = 0 , b= 0 ;
+//         int a = 0 , b= 0 ;
         
-        ListNode *cur = headA ; 
-        while(cur)
-        {
-            a++ ; 
-            cur = cur->next ; 
-        }
+//         ListNode *cur = headA ; 
+//         while(cur)
+//         {
+//             a++ ; 
+//             cur = cur->next ; 
+//         }
         
-        cur= headB ; 
-        while(cur)
-        {
-            b++ ; 
-            cur = cur->next ; 
-        }
+//         cur= headB ; 
+//         while(cur)
+//         {
+//             b++ ; 
+//             cur = cur->next ; 
+//         }
         
-        int diff = abs(a - b) ; 
+//         int diff = abs(a - b) ; 
         
-        if(a < b)
-        {
-            while(diff--)
-                headB = headB->next ; 
-        }
-        else
-        {
-            while(diff--)
-                headA = headA->next ;
-        }
+//         if(a < b)
+//         {
+//             while(diff--)
+//                 headB = headB->next ; 
+//         }
+//         else
+//         {
+//             while(diff--)
+//                 headA = headA->next ;
+//         }
         
-        while(headA && headB)
-        {
-            if(headA == headB)
-            {
-                return headA ; 
-            }
+//         while(headA && headB)
+//         {
+//             if(headA == headB)
+//             {
+//                 return headA ; 
+//             }
             
-            headA = headA->next ; 
-            headB = headB->next ; 
+//             headA = headA->next ; 
+//             headB = headB->next ; 
+//         }
+        
+//         return NULL ; 
+        
+        unordered_set<ListNode*> uset ; 
+        ListNode *temp = headA ;
+        
+        while(temp)
+        {
+            uset.insert(temp) ; 
+            temp = temp->next ; 
+        }
+        
+        
+        temp = headB ; 
+        while(temp)
+        {
+            if(uset.find(temp) != uset.end())
+                return temp ; 
+            else
+                temp = temp->next ; 
         }
         
         return NULL ; 
+        
+        
     }
 };
